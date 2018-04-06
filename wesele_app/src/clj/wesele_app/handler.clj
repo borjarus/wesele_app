7819786f4d9e7b8e41acc6d85cc32e3d9816fba6
Miroslaw Puczynski <mirek@sykon.pl>
2018-04-06 10:06:57 +0200

    add favicon to webpage

--- wesele_app/src/clj/wesele_app/handler.clj
+++ wesele_app/src/clj/wesele_app/handler.clj
@@ -80,7 +80,10 @@
    function gtag(){dataLayer.push(arguments);}
                    gtag('js', new Date());
 
-                   gtag('config', 'UA-112312361-1');"]]
+                   gtag('config', 'UA-112312361-1');"]
+    [:link {:rel "icon" :type "image/png" :href "img/favicon-16x16.png" :sizes "16x16"}] 
+    [:link {:rel "icon" :type "image/png" :href "img/favicon-32x32.png" :sizes "32x32"}] 
+    [:link {:rel "icon" :type "image/png" :href "img/favicon-96x96.png" :sizes "96x96"}]]
    [:body.diagonal-line-bg
     [:div {:id "app"} ""]
     (include-js "/assets/jquery/jquery.min.js"
